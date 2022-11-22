58512e2c941758e5c5e2a223e4621993b04ddf61
jianan <jianan@gmail.com>
2022-11-22 10:07:26 +0800

    toastr message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Brian2694\Toastr\Facades\Toastr;
 use App\Models\Admin;
+use App\Models\User;
 use DB;
+use Carbon\Carbon;
+use Cookie;
 
 class AdminController extends Controller
 {
@@ -33,16 +37,19 @@ class AdminController extends Controller
         if($admin > 0){
             $adminData = Admin::where(['name' => $request->name, 'password' => sha1($request->password)])->get();
             session(['adminData' => $adminData]);
+            Toastr::success('You Successfully LogIn', 'Admin Login', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
             return redirect('admin/dashboard');
         }
         else{
-            return redirect('admin/login')->with('msg', 'Invalid name/password!!');
+            Toastr::error('Invalid name/password!!', 'Wrong Info', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-bottom-right"]);
+            return redirect('admin/login');
         }
     }
 
     //Logout
     public function logout(){
         session()->forget(['adminData']);
+        Toastr::info('You have log out admin account', 'Successfully', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
         return redirect('admin/login');
     }
 
@@ -52,9 +59,69 @@ class AdminController extends Controller
         ->leftjoin('users','wallets.holder_id','=','users.id')
         ->leftjoin('transactions','wallets.id','=','transactions.wallet_id')
         ->select('wallets.*','users.name as holderName', 'transactions.type as tType', 'transactions.amount as tAmount')
+        ->orderBy('wallets.holder_id', 'asc')
+        ->orderBy('transactions.type','asc')
         ->get();
 
-        return view('admin/transactionHistory', compact('users'));
+        $counts = $users -> count();
+
+        return view('admin/transactionHistory', compact('users','counts'));
     }
 
+    //Show Profile
+    public function showProfile(){
+        return view('admin/profile');
+    }
+
+    //Show Wallet
+    public function showWallet(){
+        $wallets = DB::table('wallets')->get();
+        $users = User::all();
+
+        return view('admin/wallet', compact('wallets', 'users'));
+    }
+
+    //Deposit
+    public function deposit(Request $request){
+        $user = User::where('id',$request -> userID)->first();
+        if($user-> hasWallet('default')){
+            $wallet = $user->wallet;
+        }
+        elseif($user-> hasWallet('my-wallet')){
+            $wallet = $user->getWallet('my-wallet');
+        }
+        else{
+            Toastr::info('You do not have any wallet', 'Missing Wallet', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
+            return redirect('home');
+        }
+        $wallet -> deposit($request -> amount);
+
+        Toastr::success('Wallet Deposit RM'.$request -> amount.' Successfully', 'Deposit To '.$user -> name, ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
+        return redirect('admin/wallet');
+    }
+
+    //Withdraw
+    public function withdraw(Request $request){
+        $user = User::where('id',$request -> userID)->first();
+        $walletBalance = DB::table('wallets')->where('holder_id',$request->userID)->first();
+        if($user-> hasWallet('default')){
+            $wallet = $user->wallet;
+        }
+        elseif($user-> hasWallet('my-wallet')){
+            $wallet = $user->getWallet('my-wallet');
+        }
+        else{
+            Toastr::info('You do not have any wallet', 'Missing Wallet', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
+            return redirect('admin/wallet');
+        }
+
+        if($walletBalance->balance < $request->amount){
+            Toastr::error('You do not have enough funds to withdraw.</br> Wallet Balance: RM' .$walletBalance->balance, 'Insufficient funds', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-bottom-right"]);
+            return redirect('admin/wallet');
+        }
+
+        $wallet -> withdraw($request -> amount);
+        Toastr::success('Wallet Withdraw RM'.$request -> amount.' Successfully', 'Withdraw From '.$user -> name, ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
+        return redirect('admin/wallet');
+    }
 }
---
+++ resources/views/functionPage/toastr.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+<script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+{!! Toastr::message() !!}
